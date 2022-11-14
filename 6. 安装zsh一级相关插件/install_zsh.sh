790e3ffebf80d9f8d17e38aa201a9ba1b30e7720
yahooj2020 <62124198+yahooj2020@users.noreply.github.com>
2022-11-14 09:23:31 +0900

    Update install_zsh.sh

--- 6. 安装zsh一级相关插件/install_zsh.sh
+++ 6. 安装zsh一级相关插件/install_zsh.sh
@@ -1,20 +1,26 @@
 
 
+
 # https://blog.csdn.net/qierkang/article/details/85941316
  echo $SHELL;
  cat /etc/shells ;
  yum -y install zsh;
  chsh -s /bin/zsh;
+ yum install -y wget git ;
  wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh;
 
 
-cd ~/.oh-my-zsh/plugins
-wget http://mimosa-pudica.net/src/incr-0.2.zsh
-source incr*.zsh
+cd ~/.oh-my-zsh/plugins;
+wget http://mimosa-pudica.net/src/incr-0.2.zsh;
+source incr*.zsh;
 
 
-cd ~/.oh-my-zsh/plugins
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+cd ~/.oh-my-zsh/plugins;
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
+
+sed -i 's\robbyrussell\cypher\g' ~/.zshrc;
+sed -i 's\(git)\(git history history-substring-search node npm wd web-search last-working-dir zsh-autosuggestions)\g' ~/.zshrc;
+source ~/.zshrc;
 # vim ~/.zshrc
 # ZSH_THEME="cypher"
 # plugins=(git history history-substring-search node npm wd web-search last-working-dir zsh-autosuggestions)
@@ -33,4 +39,3 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 #下载后在配置文件的 plugins 选项的最后面添加即可
 #
 #plugins=(git history history-substring-search node npm wd web-search last-working-dir zsh-autosuggestions)
-
