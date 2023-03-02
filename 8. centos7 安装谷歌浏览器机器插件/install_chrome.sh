d042f2271e4f4c23737e4d2f94a3246f88bb09cc
ya2022j <62124198+ya2022j@users.noreply.github.com>
2023-03-02 10:40:02 +0900

    Update install_chrome.sh

--- 8. centos7 安装谷歌浏览器机器插件/install_chrome.sh
+++ 8. centos7 安装谷歌浏览器机器插件/install_chrome.sh
@@ -16,6 +16,7 @@ google-chrome --version
 https://registry.npmmirror.com/binary.html?path=chromedriver/106.0.5249.61/
 
 chmod +x chromedriver
+mv  chromedriver  /usr/local/bin
 # https://phantomjs.org/download.html 
 将解压好的文件放在/usr/bin目录下，并且给chromedrive增加可以执行权限
 
